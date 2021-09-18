27bee3fcb83132bfd37b393cb0e913323d2e33ab
Евгений <87427438+DubininEvgeny1972@users.noreply.github.com>
2021-09-18 18:55:16 +0300

    Сделано

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,3 +1,4 @@
+
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,12 +6,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    private final Egg6 egg;
 
     @Autowired
-    public Duck5(Egg6 egg) {
-        this.egg = egg;
-    }
+    private Egg6 egg;
+
     @Override
     public String toString() {
         return ", в утке яйцо " + egg.toString();
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,3 +1,4 @@
+
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,14 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private final Needle7 needle;
 
+    private Needle7 needle;
     @Autowired
     public Egg6(Needle7 needle) {
         this.needle = needle;
     }
+
     @Override
     public String toString() {
         return ", в яйце иголка " + needle.toString();
     }
+
 }
